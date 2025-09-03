9b486f8184847fb82915238a4a1c3f8a1c42ac79
DanelBarbosa <daniel.bmoreira@aluno.edu.es.gov.br>
2025-09-03 11:01:09 -0300

    Create index.php

---
+++ aula02-3trimestre/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> Aula 02 </title>
+    </head>
+    <body>
+        <?php
+            $nome="Daniel Barbosa";
+            
+            echo "Meu nome é" .$nome;
+        ?>
+    </body> 
+</html>
